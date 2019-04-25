6fda361d2e916e1f71cdb40c8bc7a817b6254e00
wangyafei07 <wangyafei07@meituan.com>
2019-04-25 20:41:58 +0800

    fix uncaughtException

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -3,5 +3,10 @@ import config from '../config';
 
 const port = config.server.port || 8090;
 
+process.on('uncaughtException', function(err) {
+  console.log(err.stack);
+  console.log('NOT exit...');
+});
+
 app.listen(port);
 console.log(`app started at port ${port}...`);
